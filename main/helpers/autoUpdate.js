d478bd560192398a035d4d0bdccd60dc629e1f60
Юрий Хоружий <urijhoruzij@gmail.com>
2023-01-13 17:06:59 +0300

    fix: edit autoUpdate

--- main/helpers/autoUpdate.js
+++ main/helpers/autoUpdate.js
@@ -3,26 +3,26 @@ const { ipcMain } = require('electron');
 
 const autoUpdate = (mainWindow) => {
 	ipcMain.on('check-update', () => {
-		mainWindow.webContents.send('update-status', 'checking-for-update');
+		mainWindow.webContents.send('update-status', { status: 'checking-for-update' });
 		autoUpdater.checkForUpdatesAndNotify();
 	});
 	autoUpdater.on('checking-for-update', () => {
-		mainWindow.webContents.send('update-status', 'checking-for-update');
+		mainWindow.webContents.send('update-status', { status: 'checking-for-update' });
 	});
 	autoUpdater.on('update-available', () => {
-		mainWindow.webContents.send('update-status', 'update-available');
+		mainWindow.webContents.send('update-status', { status: 'update-available' });
 	});
 	autoUpdater.on('update-not-available', () => {
-		mainWindow.webContents.send('update-status', 'update-not-available');
+		mainWindow.webContents.send('update-status', { status: 'update-not-available' });
 	});
 	autoUpdater.on('update-downloaded', () => {
-		mainWindow.webContents.send('update-status', 'update-downloaded');
+		mainWindow.webContents.send('update-status', { status: 'update-downloaded' });
 	});
 	autoUpdater.on('error', () => {
-		mainWindow.webContents.send('update-status', 'error');
+		mainWindow.webContents.send('update-status', { status: 'error' });
 	});
 	autoUpdater.on('download-progress', ({ bytesPerSecond, percent, total }) => {
-		mainWindow.webContents.send('update-status', 'download-progress', bytesPerSecond, percent, total);
+		mainWindow.webContents.send('update-status', { status: 'download-progress', bytesPerSecond, percent, total });
 	});
 	ipcMain.on('update', () => {
 		autoUpdater.quitAndInstall();
